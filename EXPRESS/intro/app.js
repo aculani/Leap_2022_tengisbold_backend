2d25e6a174e197eee3bfec3be0cdc72065708da8
aculani <tengisbold.pinecone@gmail.com>
2023-01-26 17:26:16 +0800

    1

--- EXPRESS/intro/app.js
+++ EXPRESS/intro/app.js
@@ -30,7 +30,7 @@ let categories = [
 
 
 
-app.get('/categories', jsonParser, (req, res) => {
+app.get('/categories', (req, res) => {
     res.status(200);
     res.json(categories);
 });
@@ -48,19 +48,31 @@ app.get('/categories/:id', (req, res) => {
 
 let nextCatId = categories.length
 
-app.delete('/categories/:id', (req, res) => {
+app.delete('/categories/:id', jsonParser, (req, res) => {
     const { id } = req.params;
     categories = categories.filter((row) => row.id !== id);
     res.json(id);
 });
 
-app.post('/categories', (req, res) => {
+app.post('/categories', jsonParser, (req, res) => {
     const { name } = req.body;
     const newCategory = { id: nextCatId++, name }
     categories.push(newCategory);
     res.send(newCategory);
-})
+});
 
+app.put('/categories/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body
+    const index = categories.findIndex((item) => item.id === Number(id));
+    if (index === -2) {
+        res.status(400).json('Bad request')
+    } else {
+        const updatedCategory = categories[index];
+        categories[index] = updatedCategory;
+        res.json(updatedCategory)
+    }
+});
 
 app.listen(port, () => {
     console.log('http://localhost:' + port)
